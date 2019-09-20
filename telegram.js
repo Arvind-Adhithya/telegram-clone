18aa9f6e8aa00311665c5c51a7d11760a4696498
Arvind-Adhithya <42761445+Arvind-Adhithya@users.noreply.github.com>
2019-09-20 12:10:49 +0530

    Bug fixes

--- telegram.js
+++ telegram.js
@@ -17,6 +17,9 @@ var person = [
     { id: 13, name: "Sabi", age: 18, email: "sa@gmail.com", phoneno: 121333, messages: [] }
 ];
 
+var conversations = []
+
+
 
 let colors = [
     "bg-primary",
@@ -36,8 +39,8 @@ var date = today.getDate() + "/" + (month + 1);
 let current = person.length;
 person.map((p, current) => {
     if (p.id != 4) {
-
-        row.innerHTML += `<div onClick="onTouch('${p.name}', ${current})" class="item position-relative align-items-center d-flex">
+        // console.log(JSON.stringify(p))
+        row.innerHTML += `<div onClick='onTouch(${JSON.stringify(p)})' class="item position-relative align-items-center d-flex">
                         <div class="d-flex align-items-center justify-content-center img-avator ${colors[random]} ">
                             <div class="font-weight-normal  text-white">
                             ${p.name.toUpperCase().slice(0, 2)}</div>
@@ -60,7 +63,8 @@ var time =
 var dateTime = date + " " + time;
 
 
-function onTouch(name, id) {
+function onTouch(payload, getConversationDetails) {
+    let userDetails = JSON.parse(JSON.stringify(payload))
     // console.log(name);
     // console.log(index);
     if (screen.width < 770) {
@@ -85,35 +89,109 @@ function onTouch(name, id) {
     // console.log(person[index]);
     // if()
 
-    let conversation = person.find((oo) => {
-        return id == oo.id
-    })
-    if (name) {
-        conversation = person.find((oo) => {
-            return name == oo.name
+    // let conversation = person.find((oo) => {
+    //     return id == oo.id
+    // })
+    // if (name) {
+    //     conversation = person.find((oo) => {
+    //         return name == oo.name
+    //     })
+    // }
+
+
+    // if (conversation) {
+    //     // console.log(conversation);
+    //     document.getElementById("message-input-area").dataset.sender = conversation.id;
+    //     document.getElementById("message-input-area").dataset.receiver = conversation.id;
+
+    //     // console.log(conversation.id);
+
+    //     document.querySelector(".chat-messages-header").innerHTML =
+    //       `
+    //     <div>
+    //         <div class="escape-back m-2" onClick="onExit(this)">
+    //             <i class="fa fa-arrow-left" aria-hidden="true"></i>
+    //         </div>
+    //         <div class=" d-flex p-1 ml-5">
+    //             <div class="d-flex flex-column pr-3">
+    //                 <div>${conversation.name}</div>
+    //             </div>
+    //             <div>${conversation.phoneno}</div>
+    //             <div class="icon-holder">
+    //                 <span id="search-icon">
+    //                     <i class="fa fa-search " aria-hidden="true"></i>
+    //                 </span>
+    //                 <span id="ellipsis">
+    //                     <i class="fa fa-ellipsis-v" aria-hidden="true"></i>
+    //                 </span>
+    //             </div>
+    //         </div>
+    //     </div>`;
+
+    //     $("#chat-body-items").html('')
+    //     conversation.messages.map((message) => {
+    //         // console.log(document.getElementById("chat-body-items"))
+    //         let messageElement = `
+    //         <div class="d-flex ${message.isSender ? 'sender' : 'receiver'}">            
+    //             <div class="d-flex align-items-center justify-content-center img-avator-message ${colors[random]} ">
+    //                 <div class=" font-weight-normal img-avator-text-message text-white">${message.owner.toUpperCase().slice(0, 1)}</div>
+    //             </div>
+
+    //             <div id="chat-highlighter">
+    //                 <h5>${message.owner}</h5>
+    //                 <input type="text" class="item_input"   value="${message.text}" disabled>
+    //             </div>
+    //         </div>`;
+    //         $("#chat-body-items").append(messageElement)
+    //         // document.getElementById("chat-body-items").appendChild = 
+    //     })
+    //     var messageBody = document.querySelector('#chat-body-items');
+    //     messageBody.scrollTop = messageBody.scrollHeight - messageBody.clientHeight;
+    // }
+    // document.getElementById("message-input-area").value = "";
+
+    // Onclicking any user from user list
+    if (!getConversationDetails) {
+        const user1 = staticActualUser.id // static user ie,. YOU > id 4
+        const user2 = userDetails.id // userId of the person whom we click from the userList
+        var conversationId
+        var conversationDetails = conversations.find(oo => {
+            return oo.between.includes(user1) && oo.between.includes(user2)
         })
+        if (conversationDetails) {
+            conversationId = conversationDetails.id
+        } else {
+
+            conversationId = conversations.length == 0 ? 1 : conversations.length + 1
+
+            let payload = {
+                id: conversationId,
+                between: [user1, user2],
+                messages: []
+            }
+            conversations.push(payload)
+            conversationDetails = payload
+        }
+        console.log('conversationDetails', conversationDetails)
+
+        document.getElementById("message-input-area").dataset.user1 = user1;
+        document.getElementById("message-input-area").dataset.user2 = user2;
+        document.getElementById("message-input-area").dataset.conversation = conversationId;
+    } else {
+        conversationDetails = getConversationDetails
     }
 
-
-    if (conversation) {
-        // console.log(conversation);
-        document.getElementById("message-input-area").dataset.sender = conversation.id;
-        document.getElementById("message-input-area").dataset.receiver = conversation.id;
-
-        // console.log(conversation.id);
-
-        document.querySelector(".chat-messages-header").innerHTML =
-            // console.log(123);
-            `
+    document.querySelector(".chat-messages-header").innerHTML =
+        `
         <div>
             <div class="escape-back m-2" onClick="onExit(this)">
                 <i class="fa fa-arrow-left" aria-hidden="true"></i>
             </div>
             <div class=" d-flex p-1 ml-5">
                 <div class="d-flex flex-column pr-3">
-                    <div>${conversation.name}</div>
+                    <div>${userDetails.name}</div>
                 </div>
-                <div>${conversation.phoneno}</div>
+                <div>${userDetails.phoneno}</div>
                 <div class="icon-holder">
                     <span id="search-icon">
                         <i class="fa fa-search " aria-hidden="true"></i>
@@ -125,121 +203,23 @@ function onTouch(name, id) {
             </div>
         </div>`;
 
-        $("#chat-body-items").html('')
-        conversation.messages.map((message) => {
-            // console.log(document.getElementById("chat-body-items"))
-            let messageElement = `
-            <div class="d-flex ${message.isSender ? 'sender' : 'receiver'}">            
+    $("#chat-body-items").html('')
+    conversationDetails.messages.map((message) => {
+
+        let messageElement = `
+            <div class="d-flex">            
                 <div class="d-flex align-items-center justify-content-center img-avator-message ${colors[random]} ">
-                    <div class=" font-weight-normal img-avator-text-message text-white">${message.owner.toUpperCase().slice(0, 1)}</div>
+                    <div class=" font-weight-normal img-avator-text-message text-white">${message.owner.name.toUpperCase().slice(0, 1)}</div>
                 </div>
                 
                 <div id="chat-highlighter">
-                    <h5>${message.owner}</h5>
+                    <h5>${message.owner.name}</h5>
                     <input type="text" class="item_input"   value="${message.text}" disabled>
                 </div>
             </div>`;
-            $("#chat-body-items").append(messageElement)
-            // document.getElementById("chat-body-items").appendChild = 
-        })
-        var messageBody = document.querySelector('#chat-body-items');
-        messageBody.scrollTop = messageBody.scrollHeight - messageBody.clientHeight;
-    }
-    document.getElementById("message-input-area").value = "";
-
-    var conversations = [
-        {
-            id: 1,
-            between: [staticActualUser.id, id],
-            messages: [
-                {
-                    text: $('#message-input-area').val(),
-                    sender: staticActualUser.id,
-                    receiver: id
-                },
-                // {
-                //     text: $('#message-input-area').val(),
-                //     sender: staticActualUser.id,
-                //     receiver: 1
-                // },
-                {
-                    text: $('#message-input-area').val(),
-                    sender: id,
-                    receiver: staticActualUser.id
-                }
-            ]
-        }
-        // ,
-        // {
-        //     id: 2,
-        //     between: [4, 2],
-        //     messages: [
-        //         {
-        //             text: "",
-        //             sender: 4,
-        //             receiver: 2
-        //         }
-        //     ]
-        // },
-    ]
-
-
-    // Onclicking any user from user list
-    const user1 = staticActualUser.id // static user ie,. YOU > id 4
-    const user2 = id // userId of the person whom we click from the userList
-    let getConversationId = conversations.find(oo => {
-        return oo.between.includes(user1) && oo.between.includes(user2)
+        $("#chat-body-items").append(messageElement)
     })
 
-    if (getConversationId) {
-        conversationId = getConversationId.id
-
-    } else {
-        let payload = {
-            id: id,
-            between: [user1, user2],
-            messages: []
-        }
-        conversations.push(payload)
-
-    }
-
-    // Onclicking the send button
-    $("#message-send-button").on('click', function () {
-        let senderId = $('#message-input-area').data('sender')
-        let receiverId = $('#message-input-area').data('receiver')
-        let conversationId = $('#message-input-area').data('conversation')
-
-        let getConversationDetails = conversations.find(oo => {
-            return oo.id == conversationId
-        })
-        getConversationDetails.push(
-            {
-                text: $('#message-input-area').val(),
-                sender: senderId,
-                receiver: receiverId
-            }
-        )
-    })
-
-    // Onclicking the receive button
-    $("#message-receive-button").on('click', function () {
-
-        let senderId = $('#message-input-area').data('receiver')
-        let receiverId = $('#message-input-area').data('sender')
-        let conversationId = $('#message-input-area').data('conversation')
-
-        let getConversationDetails = conversations.find(oo => {
-            return oo.id == conversationId
-        })
-        getConversationDetails.push(
-            {
-                text: $('#message-input-area').val(),
-                sender: senderId,
-                receiver: receiverId
-            }
-        )
-    })
 }
 function onExit(e) {
     let displayLeftHead = document.querySelector(".toggle");
@@ -252,28 +232,28 @@ function onExit(e) {
     displayRightBody.style.display = "none";
 }
 
-const makeRow = arr => {
-    let totalRows = ``;
-    arr.forEach(p => {
-        if (p.id != 4) {
-            totalRows =
-                `${totalRows} <div onClick="onTouch('${p.id}', ${current})" class="item position-relative align-items-center d-flex">
-            <div class="d-flex align-items-center justify-content-center img-avator ${colors[random]} ">
-                <div class="font-weight-normal  text-white">
-                ${p.name.toUpperCase().slice(0, 2)}</div>
-            </div>
-            <div class=" d-flex flex-column">
-                <div class="h5 m-0">${p.name}</div>
-                <div>${p.email}</div>
-            </div>
-            <div class="time position-absolute">${time}</div>
-            </div> 
-        </div>    
-    `;
-        }
-    });
-    document.getElementById("contactList").innerHTML = totalRows;
-};
+// const makeRow = arr => {
+//     let totalRows = ``;
+//     arr.forEach(p => {
+//         if (p.id != 4) {
+//             totalRows =
+//                 `${totalRows} <div onClick="onTouch('${JSON.stringify(p)}', ${current})" class="item position-relative align-items-center d-flex">
+//             <div class="d-flex align-items-center justify-content-center img-avator ${colors[random]} ">
+//                 <div class="font-weight-normal  text-white">
+//                 ${p.name.toUpperCase().slice(0, 2)}</div>
+//             </div>
+//             <div class=" d-flex flex-column">
+//                 <div class="h5 m-0">${p.name}</div>
+//                 <div>${p.email}</div>
+//             </div>
+//             <div class="time position-absolute">${time}</div>
+//             </div> 
+//         </div>    
+//     `;
+//         }
+//     });
+//     document.getElementById("contactList").innerHTML = totalRows;
+// };
 
 function finder(name) {
     const input = document.getElementById("myInput").value.toUpperCase();
@@ -287,46 +267,7 @@ function finder(name) {
     }
 }
 
-// function onEntered(e) {
-//     let forWelcome = document.querySelector(".body-greetings");
-//     forWelcome.style.display = "none";
-// }
 
-// class chatBody {
-//     constructor(itemName) {
-//         this.create(itemName);
-//     }
-
-//     create(itemName) {
-//         const msgcontainer = document.querySelector(".msg-body");
-//         let textarea = document.querySelector(".item_input");
-//         // console.log(textarea.value);
-//         textarea.value = itemName;
-
-//         textarea.disabled = true;
-//         textarea.classList.add(".msg-body");
-//         textarea.type = "text";
-
-//         let itemBox = document.querySelector(". message-holder");
-//         itemBox.classList.add("chatBody");
-
-//         // console.log(chatBody);
-//         console.log(itemBox);
-
-//         msgcontainer.appendChild(itemBox);
-
-//         // itemBox.appendChild(chatBody);
-//     }
-// }
-
-// function check() {
-//     var textarea = document.querySelector("#message-input-area");
-//     if (textarea.value != "") {
-//         new chatBody(textarea.value);
-//         textarea.value = "";
-//     }
-//     textarea.value = "";
-// }
 
 window.addEventListener("keydown", e => {
     if (e.which == 13) {
@@ -340,43 +281,90 @@ window.addEventListener("keydown", e => {
 });
 
 
-$(document).on('click', '#message-send-button', function () {
-    let userId = $('#message-input-area').data("sender")
-    // console.log(userId);
-    let conversation = person.find((oo) => {
-        return userId == oo.id
-    })
-    // console.log(conversation.id);
+// $(document).on('click', '#message-send-button', function () {
+//     let userId = $('#message-input-area').data("sender")
+//     // console.log(userId);
+//     let conversation = person.find((oo) => {
+//         return userId == oo.id
+//     })
+//     // console.log(conversation.id);
 
-    if (conversation) {
-        let payload = {
-            text: $('#message-input-area').val(),
-            owner: 'Admin',
-            isSender: true
-        }
-        conversation.messages.push(payload)
-        onTouch(null, conversation.id)
+//     if (conversation) {
+//         let payload = {
+//             text: $('#message-input-area').val(),
+//             owner: 'Admin',
+//             isSender: true
+//         }
+//         conversation.messages.push(payload)
+//         onTouch(null, conversation.id)
+//     }
+//     // if(onTouch(name,userId)){
+//     //     location.reload;
+//     // }
+//     // else{
+//     //     location.reload();
+//     // }
+// })
+
+// $(document).on('click', '#message-receive-button', function () {
+//     let userId = $('#message-input-area').data("receiver")
+//     let conversation = person.find((oo) => {
+//         return userId == oo.id
+//     })
+//     if (conversation) {
+//         let payload = {
+//             text: $('#message-input-area').val(),
+//             owner: conversation.name,
+//             isSender: false
+//         }
+//         conversation.messages.push(payload)
+//         onTouch(null, conversation.id)
+//     }
+// })
+
+// Onclicking the send button
+$("#message-send-button").on('click', function () {
+    console.log('user --2', $('#message-input-area').data('user2'))
+    let senderId = $('#message-input-area').data('user1')
+    let receiverId = $('#message-input-area').data('user2')
+    let conversationId = $('#message-input-area').data('conversation')
+
+    let getConversationDetails = conversations.find(oo => {
+        return oo.id == conversationId
+    })
+    let getParticpantDetails = person.find(oo => oo.id == $('#message-input-area').data('user2'))
+
+    let getUserDetails = person.find((oo) => oo.id == senderId)
+    let message = {
+        text: $('#message-input-area').val(),
+        owner: getUserDetails,
+        sender: senderId,
+        receiver: receiverId,
     }
-    // if(onTouch(name,userId)){
-    //     location.reload;
-    // }
-    // else{
-    //     location.reload();
-    // }
+    getConversationDetails.messages.push(message);
+    onTouch(getParticpantDetails, getConversationDetails)
 })
 
-$(document).on('click', '#message-receive-button', function () {
-    let userId = $('#message-input-area').data("receiver")
-    let conversation = person.find((oo) => {
-        return userId == oo.id
+// Onclicking the receive button
+$("#message-receive-button").on('click', function () {
+
+    let senderId = $('#message-input-area').data('user2')
+    let receiverId = $('#message-input-area').data('user1')
+    let conversationId = $('#message-input-area').data('conversation')
+
+    let getConversationDetails = conversations.find(oo => {
+        return oo.id == conversationId
     })
-    if (conversation) {
-        let payload = {
-            text: $('#message-input-area').val(),
-            owner: conversation.name,
-            isSender: false
-        }
-        conversation.messages.push(payload)
-        onTouch(null, conversation.id)
+
+    let getParticpantDetails = person.find(oo => oo.id == $('#message-input-area').data('user2'))
+
+    let getUserDetails = person.find((oo) => oo.id == senderId)
+    let message = {
+        text: $('#message-input-area').val(),
+        owner: getUserDetails,
+        sender: senderId,
+        receiver: receiverId,
     }
+    getConversationDetails.messages.push(message);
+    onTouch(getParticpantDetails, getConversationDetails)
 })
